f68e08966da777f96cf79d77454b3645558bf3d5
kgw012 <kgw012@gmail.com>
2020-07-12 21:10:03 +0900

    test

--- assets/js/list.js
+++ assets/js/list.js
@@ -1,9 +1,9 @@
 //상품리스트 넣기 js
 
-var list = document.getElementById('list');
+var list = document.getElementsByClassName('list');
 var num = 10;
 for(var i = 0; i < num; i++){
-    list.innerHTML += `
+    list[0].innerHTML += `
         <div class="box alt">
 			<!--목록-->
 				<div class="row gtr-uniform">
@@ -11,20 +11,34 @@ for(var i = 0; i < num; i++){
                         <span class="image fit" style="margin-bottom: 0;"><img src="test_image02.jpg" alt="" /></span>
                         <div class="row gtr-uniform">
                             <div class="col-11">
-                                <p id="info" style="text-align: left;">김길웅이 입은 반팔티<br/>34,900원</p>
+                                <p class="info" style="text-align: left;">김길웅이 입은 반팔티<br/>34,900원</p>
                             </div>
                             <div class="col-1" style="padding-left: 0;">
-                                <p id="info"><br/>♡</p>
+                                <p class="like"><br/>♥</p>
                             </div>
                         </div>
 					</div>
 				    <div class="col-4">
-			            <span class="image fit"><img src="test_image02.jpg" alt="" /></span>
-						<p id="info" style="text-align: left;">김길웅이 입은 반팔티<br/>34,900원</p>
+                        <span class="image fit" style="margin-bottom: 0;"><img src="test_image02.jpg" alt="" /></span>
+                        <div class="row gtr-uniform">
+                            <div class="col-11">
+                                <p class="info" style="text-align: left;">김길웅이 입은 반팔티<br/>34,900원</p>
+                            </div>
+                            <div class="col-1" style="padding-left: 0;">
+                                <p class="like"><br/>♥</p>
+                            </div>
+                        </div>
 					</div>
 					<div class="col-4">
-						<span class="image fit"><img src="test_image02.jpg" alt="" /></span>
-						<p id="info" style="text-align: left;">김길웅이 입은 반팔티<br/>34,900원</p>
+                        <span class="image fit" style="margin-bottom: 0;"><img src="test_image02.jpg" alt="" /></span>
+                        <div class="row gtr-uniform">
+                            <div class="col-11">
+                                <p class="info" style="text-align: left;">김길웅이 입은 반팔티<br/>34,900원</p>
+                            </div>
+                            <div class="col-1" style="padding-left: 0;">
+                                <p class="like"><br/>♥</p>
+                            </div>
+                        </div>
 					</div>
 				</div>
         </div>
