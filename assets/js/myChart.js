779dda30e0d5bc21aa3c650717ff3f8d7d399115
kgw012 <kgw012@gmail.com>
2020-07-13 11:26:58 +0900

    test

--- assets/js/myChart.js
+++ assets/js/myChart.js
@@ -29,7 +29,7 @@ var myChart = new Chart(ctx, {
     
     options: {
         //hover이벤트 핸들링
-        tooltips: 'enable',
+        // tooltips: 'enable',
         // onHover: function(evt, elements)
         // {   
         //     $("#myChart").css("cursor", elements[0] ? "pointer" : "default");
